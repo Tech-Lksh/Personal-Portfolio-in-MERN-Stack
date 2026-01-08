f2252c4c17a333a00552e086bee7231ff47f9cf1
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-01-08 17:45:48 +0530

    First

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,10 +1,11 @@
 const multer = require("multer");
 
-const storage = multer.memoryStorage();
-
-const upload = multer({
-  storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
+
+const upload = multer({ storage });
 
 module.exports = upload;
