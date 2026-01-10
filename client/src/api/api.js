adc85f723abc8ad308cf479b690155f4e2b48312
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-01-10 22:35:25 +0530

    Update API base URL to include '/api' path

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://personal-portfolio-in-mern-stack-6.onrender.com";
+const API_BASE_URL = "https://personal-portfolio-in-mern-stack-6.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
