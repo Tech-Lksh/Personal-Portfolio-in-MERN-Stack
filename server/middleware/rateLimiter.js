82f435fef6f7cb22b04af95da86219b2f45b35f8
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-01-11 23:40:44 +0530

    Update rateLimiter.js

--- server/middleware/rateLimiter.js
+++ server/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const rateLimiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 min
-  max: 50,                 // 5 attempts
+  max: 5,                 // 5 attempts
   message: {
     message: "Too many login attempts. Try again later."
   }
